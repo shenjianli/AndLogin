a656388bb572b16b465c1acc6f9391451062141b
jerry <shenjianli@ihuman.com>
2021-09-24 15:58:30 +0800

    增加学习注释

--- apt_annotation/src/main/java/per/wsj/annotation/RequireLogin.java
+++ apt_annotation/src/main/java/per/wsj/annotation/RequireLogin.java
@@ -4,9 +4,9 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-
+// 目标页面注解
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.SOURCE)
 public @interface RequireLogin {
-    
+    // 需要登录的Activity加上该注解
 }
